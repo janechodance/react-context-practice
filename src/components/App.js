ae1383c0b5e20c26f9dc95454aa19c2f8663266f
Jane <janechodance@gmail.com>
2022-04-21 15:32:58 -0400

    multiple contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
     state = {language: 'english'}
@@ -13,7 +15,12 @@ class App extends React.Component {
                     <i onClick={()=> this.onLanguageChange('english')} className="flag us" />
                     <i onClick={()=> this.onLanguageChange('dutch')}className="flag nl"/>
                 </div>
-                <UserCreate/>
+                <LanguageContext.Provider value={this.state.language}>
+                <ColorContext.Provider value={'red'}>
+                    <UserCreate/>
+                </ColorContext.Provider>
+                </LanguageContext.Provider>
+                
             </div>
         )
     }
