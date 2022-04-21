01bcf51dab7ccbe8feebb54e45dc800f2f881dec
Jane <janechodance@gmail.com>
2022-04-21 14:32:35 -0400

    set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
 class App extends React.Component {
+    state = {language: 'english'}
+    onLanguageChange = (language) =>{
+        this.setState({language: language})
+    }
     render(){
         return(
-            <div>App</div>
+            <div className="ui container">
+                <div>Select a language:
+                    <i onClick={()=> this.onLanguageChange('english')} className="flag us" />
+                    <i onClick={()=> this.onLanguageChange('dutch')}className="flag nl"/>
+                </div>
+                <UserCreate/>
+            </div>
         )
     }
 };
---
+++ src/components/UserCreate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+import Field from "./Field";
+
+function UserCreate(){
+    return (
+        <div className="ui form">
+            <Field/>
+            <Button/>
+
+        </div>
+    )
+}
+
+export default UserCreate
